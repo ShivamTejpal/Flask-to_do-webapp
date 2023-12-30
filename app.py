33171750c771dd192ee3e460e02154e8de675705
Shivam Tejpal <Shivamtejpal86@gmail.com>
2023-12-30 23:29:01 +0530

    Adding a working Delete button to delete a task..

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 #from flask_graphql import GraphQLView
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -16,21 +16,23 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     srno = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"{self.srno} -{self.title}"
 @app.route('/',methods=['GET','POST'])
 def hello_world():
-    if request.method=="POST":
-        print(request.form['title'])
-    todo = Todo(title="First Todo",description="Start Investing")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        todo = Todo(title=title, description=description)
+        db.session.add(todo)
+        db.session.commit()
+
     allTodo = Todo.query.all()
     return render_template('index.html',allTodo=allTodo)
-    #return 'Hello, World!'
+    
 
 @app.route('/show')
 def products():
@@ -38,7 +40,26 @@ def products():
     print(allTodo)
     return 'hello_world'
 
+@app.route('/update')
+def update():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'hello_world'
+
+@app.route('/delete/<int:srno>')
+def delete(srno):
+    todo = Todo.query.filter_by(srno=srno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect ("/")
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+#to create db
+#use from app import app,db
+#>>> with app.app_context():        
+#...     db.create_all()
